b4b94cb0ba7d3aedad84c4721e3ea251b7bf15fb
Seremontis <37476307+Seremontis@users.noreply.github.com>
2018-03-18 21:22:26 +0100

    Add files via upload

---
+++ Parzyste_nieparzyste.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parzyste_nieparzyste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int ilosc = int.Parse(Console.ReadLine());
+            string[] liczby;
+            for (int i = 0; i < ilosc; i++)
+            {
+                liczby = Console.ReadLine().Split();
+                for (int x = 1; x <= int.Parse(liczby[0]); x++)
+                {
+                    if (x%2==0)
+                    {
+                        Console.Write(liczby[x]+" ");
+                    }
+                }
+                for (int x = 1;  x <= int.Parse(liczby[0]);  x++)
+                {
+                    if (x % 2 != 0)
+                    {
+                        Console.Write(liczby[x]+" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+                
+
+           
+
+        }
+    }
+}
