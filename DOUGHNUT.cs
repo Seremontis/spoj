08910c19e1563469f841693cf0e2c28d38ef20b0
Seremontis <37476307+Seremontis@users.noreply.github.com>
2018-03-24 15:02:06 +0100

    Add files via upload

---
+++ DOUGHNUT.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_03_18
+{
+    class DOUGHNUT
+    {
+        static void Main(string[] args)
+        {
+
+            int t = Convert.ToInt32(Console.ReadLine());
+            int c, k, w;
+            for (int i = 0; i < t; i++)
+            {
+                string[] dane = Console.ReadLine().Split();
+                c = int.Parse(dane[0]);
+                k = int.Parse(dane[1]);
+                w = int.Parse(dane[2]);
+
+                if (c * w <= k)
+                    Console.WriteLine("yes");
+                else
+                    Console.WriteLine("no");            
+            }
+        }
+
+    }
+}
