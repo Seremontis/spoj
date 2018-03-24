b7d52a14fbe94f35066c4a4b30baa03eb0e69b21
Seremontis <37476307+Seremontis@users.noreply.github.com>
2018-03-24 14:34:11 +0100

    Add files via upload

---
+++ AL_26_04.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_03_18
+{
+    class AL_14_01
+    {
+        static void Main(string[] args)
+        {
+            int ilosc = Convert.ToInt32(Console.ReadLine());
+            int wiersz, licznik = 0; ;
+            
+            string[] linia;
+            for (int i = 0; i < ilosc; i++)
+            {
+                licznik = 0;
+                wiersz = Convert.ToInt32(Console.ReadLine());
+                linia = Console.ReadLine().Split();
+                for (int x = 0; x < wiersz; x++)
+                {
+                    licznik ^= Convert.ToInt32(linia[x]);
+                }
+                Console.WriteLine(licznik);
+            }
+           
+
+        }
+    }
+}
