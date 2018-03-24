399280f26b6e55125ee8cf53bf804dffb0c1b5d8
Seremontis <37476307+Seremontis@users.noreply.github.com>
2018-03-24 14:52:06 +0100

    Add files via upload

---
+++ AL_28_01.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_03_18
+{
+    class AL_26_04
+    {
+        static void Main(string[] args)
+        {
+            int ilosc = Convert.ToInt32(Console.ReadLine());
+            string slowo = Console.ReadLine();
+
+            int wynik = (ilosc / 2) ;
+            int w2=wynik;
+
+
+            do
+            {
+                for (int i = 0; i < slowo.Length; i++)
+                {
+                    if (i >= wynik && i <= w2)
+                    {
+                        Console.Write(slowo[i]);
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine();
+                wynik--;
+                w2++;
+
+            } while (wynik>=0);
+        }
+    }
+}
